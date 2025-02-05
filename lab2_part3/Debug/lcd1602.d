db90a42a532a9b17e8d34e0b99bdf0ffe0e9bd9c
Mason Ferrara <masonferrara@Masons-MacBook-Air.local>
2025-02-05 13:37:28 -0500

    first commit

---
+++ lab2_part3/Debug/lcd1602.d
@@ -0,0 +1,12 @@
+# FIXED
+
+lcd1602.o: ../lcd1602.c \
+ /Users/masonferrara/Desktop/CSC202/lab2_part3/clock.h \
+ /Users/masonferrara/Desktop/CSC202/lab2_part3/lcd1602.h \
+ /Users/masonferrara/Desktop/CSC202/lab2_part3/LaunchPad.h
+
+/Users/masonferrara/Desktop/CSC202/lab2_part3/clock.h:
+
+/Users/masonferrara/Desktop/CSC202/lab2_part3/lcd1602.h:
+
+/Users/masonferrara/Desktop/CSC202/lab2_part3/LaunchPad.h:
