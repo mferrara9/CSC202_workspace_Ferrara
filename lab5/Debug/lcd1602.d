ce79a8247fa8ff8b21e06414402e02ae0a558593
mferrara9 <mferrara9@student.monroecc.edu>
2025-03-03 11:59:54 -0500

    Lab 5 Initial Commit

---
+++ lab5/Debug/lcd1602.d
@@ -0,0 +1,12 @@
+# FIXED
+
+lcd1602.o: ../lcd1602.c \
+ /Users/masonferrara/Desktop/CSC202/csc202_workspace/clock.h \
+ /Users/masonferrara/Desktop/CSC202/csc202_workspace/lcd1602.h \
+ /Users/masonferrara/Desktop/CSC202/csc202_workspace/LaunchPad.h
+
+/Users/masonferrara/Desktop/CSC202/csc202_workspace/clock.h:
+
+/Users/masonferrara/Desktop/CSC202/csc202_workspace/lcd1602.h:
+
+/Users/masonferrara/Desktop/CSC202/csc202_workspace/LaunchPad.h:
